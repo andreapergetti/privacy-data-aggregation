2b5942fb3a119b37c82639a7f838f9c924203238
andrea <andrea@debian.com>
2023-01-05 17:51:25 +0100

    Finalized setup function

--- src/protocol.py
+++ src/protocol.py
@@ -3,19 +3,30 @@ from Crypto.Random import random
 from math import floor
 import hashlib
 
+num_part = 5
 
 def setup():
-    N = 64  # Set to 64 just for faster testing (in real scenario use 1028 or 2048)
+    N = 16  # Set to 16 just for faster testing (in real scenario use 1028 or 2048)
     while True:
         p = number.getPrime(N=N)
-        print(p)
+        # print(p)
         q = 2*p+1
-        print(q)
+        # print(q)
         if number.isPrime(q):
             break
-    generator = ((random.randrange(floor(q/2))) ^ 2) % q
-    print(f'Generator: {generator}')
-    return q
+    generator = ((random.randrange(floor(q/2) + 1)) ^ 2) % q
+    # print(f'Generator: {generator}')
+    s = []
+    while True:
+        for i in range(num_part+1):
+            s.append(random.randrange(p))
+        # print(f'Secret random {s}')
+        # print(f'Is zero {sum(s) % p}   {p}')
+        if sum(s) % p == 0:
+            break
+        else:
+            s = []
+    return generator, s, q
 
 
 def hash_func(x, q):
@@ -27,5 +38,9 @@ def hash_func(x, q):
     return num
 
 
-q = setup()
-hash_func(996, q)
+s = []
+generator, s, q = setup()
+print(f'Generator {generator}')
+print(f'Random secrets {s}')
+print(f'Prime number q {q}')
+#hash_func(996, q)
